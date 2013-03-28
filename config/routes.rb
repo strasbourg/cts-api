2f510c50dbb4a2ef9d0e826af6cb72102da2c468
Paul Chobert <paul@chobert.fr>
2013-03-28 16:39:20 +0100

    redirect root to /stops

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SxbDataMissingApi::Application.routes.draw do
   scope module: :api, as: :api, subdomain: :api do
     scope module: :cts, as: :cts, subdomain: :cts do
+      root to: redirect('/stops')
       scope module: :v1, format: :json do
         resources :stops, only: [:index, :show] do
           resources :arrivals
