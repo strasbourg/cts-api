f882e01af6ac88eac77558a0db79ed3af43bed0d
Paul Chobert <paul@chobert.fr>
2013-04-02 15:21:06 +0200

    log requests

--- app/controllers/api/cts/v1/api_cts_v1_controller.rb
+++ app/controllers/api/cts/v1/api_cts_v1_controller.rb
@@ -1,8 +1,25 @@
+require 'net/http'
+require 'uri'
+
 class Api::Cts::V1::ApiCtsV1Controller < ApplicationController
-  log_request scope: 'cts:v1'
+  prepend_before_filter :log_request
 
   protected
   def current_ability
     @_current_ability ||= Cts::Ability.new
+  end
+
+  def log_request
+    Thread.new do
+      Net::HTTP.post_form URI('http://stats.strasbourg-data.fr/track'),
+        {
+          password: ENV['LOGGER_PASSWORD'],
+          ip: request.env['HTTP_X_FORWARDED_FOR'],
+          service: 'cts',
+          path: request.path,
+          params: params.except('controller', 'action', 'format', 'subdomain').to_query,
+          time: Time.now.iso8601(3)
+        }
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,4 @@ class ApplicationController < ActionController::Base
       password == ENV['PROTECTION_PASSWORD']
     end
   end
-
-  def self.log_request(scope: nil)
-    instance_eval do
-      before_filter do |c|
-        ApiRequest.create ip_address: request.remote_ip, params: params.permit(:id, :controller, :action)
-      end
-    end
-  end
 end
